833892c8bd183b93e4918e3b01f9e7ca8040d0ab
alberto.donati6 <alberto.donati6@studio.unibo.it>
2021-03-14 16:31:21 +0100

    fine ex8

--- src/myLAB2/EX8.scala
+++ src/myLAB2/EX8.scala
@@ -29,57 +29,15 @@ object EX8 {
       case _ => None()
     }
 
-
-      //questi boh?
-    //case class None[B]() extends Option[B]
-    //case class Some[B](b: B) extends Option[B]
-
-
-    // pure questi cosa sono ??
-
-/*
-    def map3[A](opt3: Some[A])(predicate: A => Some(Boolean): Some[Boolean] = opt3 match {
-      case Some(a) if(predicate(a)) => Some(Boolean)
-      case _ => null
-    }*/
-
-    /*
-    def map3[A,B](opt3: Some(a))(f: Some[A]=> Some[a]): Some[Boolean] = opt3 match {
-       case Some(a) if (Some(a)== true) => Some(true)
-         case _ => null
-
-    }*/
-
-    def map[A,B] (opt2: Some[A])(predicate: Option[A] => Some[B]): Option[A] = opt2 match {
-      case Some(a) => Some[B]
+    def map[A](opt3: Option[A])(opt4: A => Boolean): Option[Boolean] = opt3 match {
+      case Some(a) if opt4(a) => Some(true)
       case _ => None()
     }
 
-
-    case class Some2[B](b: B) extends Option[B]
-
-      def map2[A,B] (opt3: Some[A] => Boolean)(opt4: Some2[B] => Boolean): (Option[A],Option[B]) = (opt3,opt4) match {
-      //  case Some(a),Some(b) if ((opt3) || (opt4)) => Some[a],Some(b)
-
-        case (Some(a),Some2(b)) if (opt3==opt4) => (Some(a),Some2(b))
-        case _ => null
-      }
-
-    def map3[A,B] (opt3: Some[A] => Boolean)(opt4: Some2[B] => Boolean): (Option[A],Option[B]) = (opt3,opt4) match {
-      //  case Some(a),Some(b) if ((opt3) || (opt4)) => Some[a],Some(b)
-
-      case (Some(a),Some2(b)) if (opt3==true && opt4==true) => (Some(a),Some2(b))
-      case _ => null
+    def map2[A](opt5: Option[A])(opt6: Option[A])(f: (Option[A],Option[A]) => Option[A]): Option[A] = (opt5,opt6) match {
+      case (opt5,opt6) if ( opt5==None() || opt6==None()) => None()
+      case _ => f(opt5,opt6)
     }
-
-    def map4[A,B] (opt3: Some[A] => Boolean)(opt4: Some[B] => Boolean): (Option[A],Option[B]) = (opt3,opt4) match {
-      //  case Some(a),Some(b) if ((opt3) || (opt4)) => Some[a],Some(b)
-
-      case (Some(a),Some2(b)) if (opt3==true && opt4==true) => (Some(a),Some2(b))
-      case _ => null
-    }
-
-
 
   }
 
--- test/myLAB2Test/EX8Test.scala
+++ test/myLAB2Test/EX8Test.scala
@@ -1,26 +1,24 @@
 package myLAB2Test
 
+import myLAB2.EX8.Option._
 import org.junit.jupiter.api.Assertions._
 import org.junit.jupiter.api.Test
-import myLAB2.EX8.Option._
 
 class EX8Test {
 
   @Test def testOptionFilter() {
-    assertEquals(Some(5),filter[Int](Some(5))(_>2))
-    assertEquals(None(),filter[Int](Some(5))(_>8))
+    assertEquals(Some(5), filter[Int](Some(5))(_ > 2))
+    assertEquals(None(), filter[Int](Some(5))(_ > 8))
   }
 
-  /*
-
   @Test def testOptionMap() {
-    assertEquals(Some(true), map (Some(5)) (_>2) )
+    assertEquals(Some(true), map[Int](Some(5))(_ > 2))
+    assertEquals(None(), map[Int](None[Int])(_ > 2))
   }
 
   @Test def testOptionMap2() {
-     map4 (Some(5)>2) (Some(4)>2) )
+    assertEquals(Some(5), map2(Some(5))(Some(2))((value, _) => value))
+    assertEquals(None(), map2(Some(5))(None())((value, _) => value))
   }
-
-   */
 
 }
